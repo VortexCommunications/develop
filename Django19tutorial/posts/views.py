ce298e7a2f24bf7a246e766b8c13ab7effc893db
tabuu9 <ericfborges@gmail.com>
2017-06-26 13:25:05 -0400

    Update 1.1
    Templates (with context!), as well as URLs.

--- Django19tutorial/posts/views.py
+++ Django19tutorial/posts/views.py
@@ -4,5 +4,41 @@ from django.shortcuts import render
 # Create your views here.
 
 
-def posts_home(request):
-	return HttpResponse("<h1>Hello!</h1>")
+def post_create(request):
+	context = {
+	"title": "create"
+	}
+	return HttpResponse("<h1>Create!</h1>")
+
+
+
+def post_detail(request):
+	context = {
+	"title": "detail"
+	}
+	return render(request, "index.html", context)
+
+def post_list(request):
+	if request.user.is_authenticated():
+		context = {
+			"title": "personal list"
+		}
+		return render(request, "index.html", context)
+	else:
+		context = {
+			"title": "list"
+		}
+		return render(request, "index.html", context)
+	#return HttpResponse("<h1>List!</h1>")
+
+def post_update(request):
+	context = {
+	"title": "update"
+	}
+	return HttpResponse("<h1>Update!</h1>")
+
+def post_delete(request):
+	context = {
+	"title": "Delete"
+	}
+	return HttpResponse("<h1>Delete!</h1>")
